433f548b5e4ea02080aa71b0dc9e401da3a608a0
Onur KINAY <ben@onurkinay.com.tr>
2024-09-21 19:36:46 +0300

    controller group and view

--- routes/web.php
+++ routes/web.php
@@ -3,19 +3,16 @@
 use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
+Route::view("/", 'home');
+Route::view('/contact', 'contact');
+
+Route::controller(JobController::class)->group(function (){
+    Route::get("/jobs", "index");
+    Route::get('/jobs/create', 'create');
+    Route::post('/jobs', 'store');
+    Route::get('/jobs/{id}', 'show');
+    Route::get('/jobs/{id}/edit', 'edit');
+    Route::patch('/jobs/{id}', 'update');
+    Route::delete('/jobs/{id}', 'delete');
 });
 
-Route::get("/jobs", [JobController::class,"index"]);
-Route::get('/jobs/create', [JobController::class,'create']);
-Route::post('/jobs', [JobController::class,'store']);
-Route::get('/jobs/{id}', [JobController::class,'show']);
-Route::get('/jobs/{id}/edit', [JobController::class,'edit']);
-Route::patch('/jobs/{id}', [JobController::class,'update']);
-Route::delete('/jobs/{id}', [JobController::class,'delete']);
-
-
-Route::get('/contact', function () {
-    return view('contact');
-});
