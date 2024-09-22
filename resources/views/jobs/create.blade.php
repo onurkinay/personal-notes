f2265d7765b441d652694b8e66f0dca4fb0bddb1
Onur KINAY <ben@onurkinay.com.tr>
2024-09-22 12:19:36 +0300

    form made components

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -12,38 +12,28 @@
                 <p class="mt-1 text-sm leading-6 text-gray-600">We just need a handful of details from you.</p>
 
                 <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                    <div class="sm:col-span-4">
-                        <label for="title" class="block text-sm font-medium leading-6 text-gray-900">Title</label>
-                        <div class="mt-2">
-                            <div
-                                class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                <input type="text" name="title" id="title"
-                                    class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                                    placeholder="Shift Leader">
-                            </div>
-                            @error('title')
-                            <p class="text-xs text-red-500 font-semibold mt-1"> {{$message}}</p> 
-                            @enderror
-                        </div>
-                    </div>
+                    <x-form-field>
+                        <x-form-label for="title">Title</x-form-label>
 
-                    <div class="sm:col-span-4">
-                        <label for="salary" class="block text-sm font-medium leading-6 text-gray-900">Salary</label>
                         <div class="mt-2">
-                            <div
-                                class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                <input type="text" name="salary" id="salary"
-                                    class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                                    placeholder="$50,000 Per Year">
-                            </div>
-                            @error('salary')
-                            <p class="text-xs text-red-500 font-semibold mt-1"> {{$message}}</p> 
-                            @enderror
+                            <x-form-input type="text" name="title" id="title" placeholder="CEO"
+                                required></x-form-input>
+                            <x-form-error name="title" />
                         </div>
-                    </div>
-              
+                    </x-form-field>
+
+                    <x-form-field>
+                        <x-form-label for="Salary">Salary</x-form-label>
+
+                        <div class="mt-2">
+                            <x-form-input type="text" name="salary" id="salary" placeholder="50000"
+                                required></x-form-input>
+                            <x-form-error name="salary" />
+                        </div>
+                    </x-form-field>
+
                 </div>
-             <?php /*  <div class="mt-10">
+                <?php /*  <div class="mt-10">
                   @if ($errors->any())
                   <ul>
                       @foreach ($errors->all() as $error)
@@ -52,8 +42,9 @@
                   </ul>
   
               @endif  
-                </div>  */?>
-              
+                </div>  */
+                ?>
+
             </div>
 
 
@@ -62,8 +53,7 @@
 
         <div class="mt-6 flex items-center justify-end gap-x-6">
             <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
-            <button type="submit"
-                class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+            <x-form-button>Submit</x-form-button>
         </div>
     </form>
 
