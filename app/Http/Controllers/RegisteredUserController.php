5c1d5694450bcef0ad6e31a8d93c6a1d16af2628
Onur KINAY <ben@onurkinay.com.tr>
2024-09-22 15:39:27 +0300

    create user and login

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\Password;
+use App\Models\User; 
+use Illuminate\Support\Facades\Auth;
 
 class RegisteredUserController extends Controller
 {
@@ -11,6 +14,18 @@ class RegisteredUserController extends Controller
     }
 
     public function store(){
-       dd(request()->all());
+      $validatedAttributes = request()->validate([
+        'name' => ['required'],
+        'email' => ['required', 'email','max:254'],
+        'password'=> ['required', Password::min(5),'confirmed'],
+        'password_confirmation' => ['required'],
+
+        ]);
+
+        $user = User::create( $validatedAttributes );
+
+        Auth::login($user);
+
+        return redirect('jobs');
     }
 }
